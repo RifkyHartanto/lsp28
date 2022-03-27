609aa262155ba1e40fa1e05f6ef9d0d4f663b68d
Bayu Aji <57161957+bayuaji724@users.noreply.github.com>
2022-03-27 16:02:12 +0700

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -3,29 +3,13 @@ def garis():
 
 #Fungsi Bubble Sort Ascending(Dari kecil ke besar)
 def sort_asc(array):
-    n = len(array) #n itu adalah jumlah baris
-    #perulangan pertama
-    for i in range(n):
-        #perulangan kedua
-        for j in range(n-i-1):
-            #membandingkan masing" elemen ke kanan
-            if array[j] > array[j+1]:
-                #jika lebih besar, tukar ke kiri
-                array[j], array[j+1] = array[j+1], array[j]
-    return array
+    array = sorted(array)
+    return(array)
 
 #fungsi bubble sort descending (Dari besar ke kecil)
 def sort_desc(array):
-    n = len(array) #n itu adalah jumlah baris
-    #perulangan pertama
-    for i in range(n):
-        #perulangan kedua
-        for j in range(n-i-1):
-            #membandingkan masing" elemen ke kanan
-            if array[j] < array[j+1]:
-                #jika lebih besar, tukar ke kiri
-                array[j], array[j+1] = array[j+1], array[j]
-    return array
+    array = sorted(array, reverse = True)
+    return(array)
 
 #input nilai
 garis()
@@ -42,7 +26,7 @@ def rata_rata(angka):
     return sum(angka)/len(angka)
 
 #nilai akhir
-print("HASIL AKHIR------")
+print("-----HASIL AKHIR------")
 print("Urutan angka ascending : ",(sort_asc(angka)))
 print("Urutan angka descending : ",(sort_desc(angka)))
 
